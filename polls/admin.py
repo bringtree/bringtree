d3770f0108acad11734e27708f78513f59b01fe4
bringtree <bringtree@qq.com>
2017-04-22 12:46:06 +0800

    加入2个模版

--- polls/admin.py
+++ polls/admin.py
@@ -8,9 +8,10 @@ from .models import Question, Choice
 
 # Register your models here.
 
-
-class ChoiceInline(admin.StackedInline):
+#改用TabularInline 不用StackedInline 来使得页面 更加紧凑
+class ChoiceInline(admin.TabularInline):
     model = Choice
+    #每次打开admin页面中的admin中choice 的时候 会自动多出三个选项来选择
     extra = 3
 
 
@@ -20,6 +21,10 @@ class QuestionAdmin(admin.ModelAdmin):
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
+    #pills 中的questions页面中显示的信息
+    list_display = ('question_text','pub_date','was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 
 
 admin.site.register(Question, QuestionAdmin)
